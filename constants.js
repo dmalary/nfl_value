ba75f2593331fb14ca2c80c6afd7212d11a3a743
madhat5 <davidmalary@gmail.com>
2024-03-15 09:50:05 -0400

    feat: init commit

---
+++ constants.js
@@ -0,0 +1,5 @@
+export const years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023'];
+
+export const teams = [ 'crd', 'atl', 'rav', 'buf', 'car', 'chi', 'cin', 'cle', 'dal', 'den', 'det', 'gnb', 'htx', 'clt', 'jax', 'kan', 'rai', 'sdg', 'ram', 'mia', 'min', 'nwe', 'nor', 'nyg', 'nyj', 'phi', 'pit', 'sfo', 'sea', 'tam', 'oti', 'was']
+
+export default {};
